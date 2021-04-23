c7aff4be364eb5302acd58757578e3f3944f388a
bruno-florentino <umbrunoflorentino@gmail.com>
2021-04-23 13:38:16 -0300

    inclusao da pagina do linkedin

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/Link'
 function Home(){
-    return <div><h2>Florentino</h2> <h1>BRUNO</h1></div>
+    return (
+        <div>
+            <Link href="https://www.linkedin.com/in/bruno-florentino-680b6421/">
+                <h1>
+                    <a>Bruno Florentino</a>
+                </h1>
+            </Link>
+        </div>
+    )
 }
 
 export default Home
